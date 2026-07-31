12ecf7015e34f04e3676096f7f09757f30ae6d6a
Enrico Brosque Palma <enricob.palma@gmail.com>
2026-07-31 14:58:26 -0300

    fix():fixes main file for better agnostic infra

--- main.tf
+++ main.tf
@@ -1,10 +1,14 @@
-resource "azurerm_resource_group" "dev" {
-  name     = "rg-${var.application_name}-${var.environment}"
-  location = var.location
-
-  tags = {
+locals {
+  common_tags = {
     application = var.application_name
     environment = var.environment
     managed_by  = "terraform"
   }
+}
+
+resource "azurerm_resource_group" "application" {
+  name     = "rg-${var.application_name}-${var.environment}"
+  location = var.location
+
+  tags = local.common_tags
 }
